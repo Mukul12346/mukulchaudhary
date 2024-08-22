8a57aed0c520a4b3f46f2cc534ab9f1d411a3183
Mukul12346 <154653666+Mukul12346@users.noreply.github.com>
2024-08-22 10:32:24 +0530

    Add files via upload

---
+++ leftbar.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="keyword" content="resume,cv,portfolio,vcard" />
+      <title>mukul/Portfolio</title>
+      <!-- favicon -->
+      <link
+         rel="shortcut icon"
+         href="assets/images/favicon.png"
+         type="image/x-icon"
+         />
+      <!-- bootstrap -->
+      <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+      <!-- Plugin css -->
+      <link rel="stylesheet" href="assets/css/plugin.css" />
+      <!-- Flaticon -->
+      <link rel="stylesheet" href="assets/css/flaticon.css" />
+      <!-- stylesheet -->
+      <link rel="stylesheet" href="assets/css/style.css" />
+      <link rel="stylesheet" href="assets/css/multicolor.css" />
+      <!-- responsive -->
+      <link rel="stylesheet" href="assets/css/responsive.css" />
+   </head>
+   <style>
+    html {
+  scroll-behavior: smooth;
+}
+   </style>
+   <body>
+      <!-- preloader area start -->
+      <!-- <div class="preloader" id="preloader">
+         <div class="loader loader-1">
+           <div class="loader-outter"></div>
+           <div class="loader-inner"></div>
+         </div>
+         </div> -->
+      <!-- preloader area end -->
+      <!--Main-Menu Area Start-->
+      <div class="side-menu-wrapper">
+         <div class="menu-toogle-icon">
+            <div id="nav-icon3">
+               <span></span>
+               <span></span>
+               <span></span>
+               <span></span>
+            </div>
+         </div>
+         <div class="side-menu">
+            <div class="heading-area">
+               <a href="index.html" class="profile-photo">
+               <img
+                  src="assets/images/1.jpg"
+                  alt=""
+                  class="wow zoomIn"
+                  data-wow-duration="0.8s"
+                  data-wow-delay="0.2s"
+                  />
+               </a>
+               <div
+                  class="name wow fadeInUp"
+                  data-wow-duration="0.8s"
+                  data-wow-delay="0.3s"
+                  >
+                  MUKUL
+               </div>
+            </div>
+            <ul id="mainmenu-area">
+               <li class="current">
+                  <a
+                     href="#home"
+                     class="wow fadeInUp"
+                     data-wow-duration="0.8s"
+                     data-wow-delay="0.4s"
+                     ><i class="fas fa-home"></i>Home</a
+                     >
+               </li>
+               <li>
+                  <a
+                     href="#about"
+                     class="wow fadeInUp"
+                     data-wow-duration="0.8s"
+                     data-wow-delay="0.4s"
+                     ><i class="fas fa-user"></i>About</a
+                     >
+               </li>
+               <li>
+                  <a
+                     href="#service"
+                     class="wow fadeInUp"
+                     data-wow-duration="0.8s"
+                     data-wow-delay="0.4s"
+                     ><i class="fas fa-briefcase"></i>Services</a
+                     >
+               </li>
+               <li>
+                  <a
+                     href="#resume"
+                     class="wow fadeInUp"
+                     data-wow-duration="0.8s"
+                     data-wow-delay="0.4s"
+                     ><i class="fas fa-file-alt"></i>Resume</a
+                     >
+               </li>
+               <li>
+                  <a
+                     href="#project-gallery"
+                     class="wow fadeInUp"
+                     data-wow-duration="0.8s"
+                     data-wow-delay="0.4s"
+                     ><i class="fas fa-layer-group"></i>Project</a
+                     >
+               </li>
+               <li>
+                  <a
+                     href="#contact"
+                     class="wow fadeInUp"
+                     data-wow-duration="0.8s"
+                     data-wow-delay="0.4s"
+                     ><i class="fab fa-whatsapp"></i>Contact</a
+                     >
+               </li>
+            </ul>
+         </div>
+      </div>
+      <!--Main-Menu Area Start-->
+   </body>
+</html>
